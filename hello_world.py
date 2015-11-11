24c1dab292c738ff4b5718b434a467d3edd869b4
michaelawyu <michael.a.w.yu@hotmail.com>
2015-11-11 05:14:03 -0500

    Test

--- hello_world.py
+++ hello_world.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/test')
+def test():
+	testList=[42,50,12,25,26,37]
+	return render_template('test.html',testList=testList)
+
 if __name__=='__main__':
 	app.debug=True
 	app.run(host='0.0.0.0')
